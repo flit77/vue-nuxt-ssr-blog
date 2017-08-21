eb093fc42bdf8762ebeaa80892d8c0f0fd52d289
Vasiliy Shubko <flit77@gmail.com>
2017-08-21 09:44:08 +0200

    css framework added

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,19 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ssr-blog',
+    title: '%s | SSR Blog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css'
+      }
     ]
   },
   /*
@@ -24,15 +29,15 @@ module.exports = {
     /*
     ** Run ESLINT on save
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
       if (ctx.dev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
